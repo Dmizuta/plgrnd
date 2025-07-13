3befb3403bd07964e4ccbcb99df8b41774eda1c4
Dani Mizuta <dani.mizuta@gmail.com>
2025-07-13 18:47:28 -0300

    feat: railway

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-window.BACKEND_URL = "https://plgrnd-production.up.railway.app/"
+window.BACKEND_URL = "https://nyrfestas-bcknd-module-production.up.railway.app"
 
-//window.BACKEND_URL = "http://localhost"
+
+//window.BACKEND_URL = "http://localhost:3000"
 
 const URL_API = window.BACKEND_URL;
